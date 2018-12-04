2129a49ba6e58438aee75dd5949a272bb1283bf8
NovaFox161 <novamaday@gmail.com>
2018-12-04 16:35:33 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# CodeName "T.A.P"
+Tumblr Alternative Platform -  A blogging platform for all.
+
+This is the core code that handles universal Objects and methods.
+
+> More info coming soon.
---
+++ src/main/java/org/dreamexposure/tap/core/TapCore.java
@@ -0,0 +1,12 @@
+package org.dreamexposure.tap.core;
+
+/**
+ * @author NovaFox161
+ * Date Created: 12/4/18
+ * For Project: TAP-Core
+ * Author Website: https://www.novamaday.com
+ * Company Website: https://www.dreamexposure.org
+ * Contact: nova@dreamexposure.org
+ */
+public class TapCore {
+}
